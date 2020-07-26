2b435fdfa0156fdec2664a06a5f52cb60c0b45a9
AlexSPR582 <52915592+AlexSPR582@users.noreply.github.com>
2020-07-27 00:29:56 +0300

    Delete Main.java

--- study-bsac-kiap/src/pz/pz4/Main.java
+++
@@ -1,9 +0,0 @@
-package pz.pz4;
-
-public class Main {
-    public static void main(String[] args) {
-        String str = " 5ddfhgjkdf6 rfs1";
-        StringTask1 task1 = new StringTask1();
-        System.out.println(task1.countSumNumbersInString(str));
-    }
-}
